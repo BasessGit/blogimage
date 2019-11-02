a9eeb4cddc204d142c3416b527d05bd8979e9259
xiaowei <1690471720@qq.com>
2019-11-02 11:06:33 +0800

    imageblog第三次修改

---
+++ blogimage/src/main/java/cn/wuaijing/controller/TestController.java
@@ -0,0 +1,34 @@
+package cn.wuaijing.controller;
+
+import cn.wuaijing.bean.User;
+import com.alibaba.fastjson.JSONPObject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/xiaoming")
+    @ResponseBody
+    public JSONPObject  getJson(){
+        List<Object> list = new ArrayList<>();
+        User user = new User(null,null,new Date(),null);
+        JSONPObject jsonpObject = new JSONPObject("测试");
+        jsonpObject.addParameter(user);
+        String json = jsonpObject.toJSONString();
+        System.out.println(json);
+      list = jsonpObject.getParameters();
+      for(Object o :list){
+          User user1 =(User) o;
+          System.out.println(user1.getDate());
+      }
+
+        return jsonpObject;
+    }
+
+}
