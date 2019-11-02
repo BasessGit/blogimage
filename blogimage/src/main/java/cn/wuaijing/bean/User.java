60768c362209cc67b7892291d1b7711fed372d2b
xiaowei <1690471720@qq.com>
2019-11-02 11:07:47 +0800

    imageblog第三次修改

---
+++ blogimage/src/main/java/cn/wuaijing/bean/User.java
@@ -0,0 +1,62 @@
+package cn.wuaijing.bean;
+
+import java.util.Date;
+
+public class User {
+    private Integer id;
+    private String name;
+    private Date date;
+    private Boolean  isTrue;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, Date date, Boolean isTrue) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.isTrue = isTrue;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean isTrue() {
+        return isTrue;
+    }
+
+    public void setTrue(Boolean aTrue) {
+        isTrue = aTrue;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", isTrue=" + isTrue +
+                '}';
+    }
+}
